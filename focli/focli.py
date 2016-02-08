0d9f1e10268c4276465b81e5da030acee8b0aabc
Joona Hoikkala <joona@kuori.org>
2016-02-08 08:15:52 +0200

    Main (and output) class

--- focli/focli.py
+++ focli/focli.py
@@ -2,6 +2,41 @@ import datetime
 import json
 
 import requests
+from prettytable import PrettyTable
+
+
+class FoliPrint:
+    def __init__(self, flines=[]):
+        self.stops = []
+        if flines:
+            for l in flines:
+                new_stop = FoliStop(l)
+                new_stop.run()
+                self.stops.append(new_stop)
+
+    def print_lines(self):
+        for s in self.stops:
+            journeys = self.filter_list(s.journeys)
+            table = PrettyTable(["Time", "Line", "Difference"])
+            for jo in journeys:
+                f_timediff = datetime.timedelta(minutes=int(jo.timediff))
+                line_time = jo.time + f_timediff
+                f_time = "{0:02d}:{1:02d}".format(line_time.hour,
+                                                  line_time.minute)
+                table.add_row([
+                    f_time,
+                    jo.name,
+                    jo.timediff])
+            table.sort_key("Time")
+            print(table)
+
+    def filter_list(self, flist, start_min=-5, end_min=60):
+        now = datetime.datetime.now()
+        start_delta = datetime.timedelta(minutes=start_min)
+        end_delta = datetime.timedelta(minutes=end_min)
+        start = now + start_delta
+        end = now + end_delta
+        return [f for f in flist if f.time > start and f.time < end]
 
 
 class FoliLine:
