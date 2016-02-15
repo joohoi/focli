4465deb0774c4f1d68357dc3a42b3bb79e3ff2ce
Joona Hoikkala <joona@kuori.org>
2016-02-15 19:28:17 +0200

    focli.py tests 100% coverage

--- focli/tests/focli_test.py
+++ focli/tests/focli_test.py
@@ -78,14 +78,17 @@ class TestFocli:
 
         assert focli.main() == 0
 
-
-
-
     def test_read_config(self, monkeypatch):
         fcontent = '{"123":"abc"}'
         with mock.patch.object(builtins, 'open',
                                mock.mock_open(read_data=fcontent)):
             assert focli.read_config() == json.loads(fcontent)
+
+    def test_read_config_exception(self, monkeypatch):
+        monkeypatch.setattr('focli.focli.CONFIG_PATH',
+                            '/does/not/exist/for/sure')
+
+        assert focli.read_config() == dict()
 
     def test_write_config(self, monkeypatch):
         fcontent = json.loads('{"123":"abc"}')
@@ -94,3 +97,59 @@ class TestFocli:
             focli.write_config(fcontent)
         mo.assert_called_once_with(
             os.path.expanduser(focli.CONFIG_PATH), 'w')
+
+    def test_write_config_exception(self, monkeypatch):
+        with mock.patch.object(builtins, 'open',
+                               mock.mock_open()):
+            with pytest.raises(TypeError):
+                focli.write_config(set())
+
+    def test_add_bookmark(self, monkeypatch):
+        fcontent = '{"123":"abc"}'
+        with mock.patch.object(builtins, 'open',
+                               mock.mock_open(read_data=fcontent)):
+            monkeypatch.setattr('sys.argv', ['focli', '-a', '157'])
+            assert focli.main() == 0
+
+            monkeypatch.setattr('sys.argv', ['focli', '-a'])
+            assert focli.main() == 1
+
+    def test_del_bookmark(self, monkeypatch):
+        fcontent = '{"123":"abc"}'
+        with mock.patch.object(builtins, 'open',
+                               mock.mock_open(read_data=fcontent)):
+            monkeypatch.setattr('sys.argv', ['focli', '-d', '123'])
+            assert focli.main() == 0
+
+            monkeypatch.setattr('sys.argv', ['focli', '-d', '157'])
+            assert focli.main() == 1
+
+            monkeypatch.setattr('sys.argv', ['focli', '-d'])
+            assert focli.main() == 1
+
+    def test_list_bookmarks(self, monkeypatch):
+        fcontent = '{"123":"abc"}'
+        with mock.patch.object(builtins, 'open',
+                               mock.mock_open(read_data=fcontent)):
+            monkeypatch.setattr('sys.argv', ['focli', '-l'])
+            assert focli.main() == 0
+
+    def test_show_stops(self, monkeypatch):
+        def mock_init(*args, **kwargs):
+            return None
+
+        def mock_func(*args):
+            return True
+
+        monkeypatch.setattr('focli.foline.FoliPrint.__init__', mock_init)
+        monkeypatch.setattr('focli.foline.FoliPrint.print_lines', mock_func)
+        monkeypatch.setattr('sys.argv', ['focli', '157'])
+        assert focli.main() == 0
+        fcontent = '{"123":"abc"}'
+        monkeypatch.setattr('sys.argv', ['focli'])
+        with mock.patch.object(builtins, 'open',
+                               mock.mock_open(read_data=fcontent)):
+            assert focli.main() == 0
+        with mock.patch.object(builtins, 'open',
+                               mock.mock_open(read_data="{}")):
+            assert focli.main() == 1
